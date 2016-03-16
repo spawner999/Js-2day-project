53a98e6567c02ecdf978056b78070f99240228f8
Joey Arnstein and lorenzo ferrario <student@epicodus.com>
2016-03-16 15:35:16 -0700

    initial files, working apis, day one in the books

--- js/main-interface.js
+++ js/main-interface.js
@@ -3,14 +3,16 @@ var clientId = '44ZBUPBKJLBXBM0IJRZPSM34YPUKNSE2VQCNL041GNFIWRSO';
 var clientKey = 'RPHMMDSE0DQIAY4XZ34WN1ZRTUV250NCNIX05N1WMSRODPJ2';
 var template = 'https://api.foursquare.com/v2/venues/explore?client_id=%CLIENT_ID%&client_secret=%CLIENT_SECRET%&v=20130815&near=%CITY%&query=%query%'
 var Venue = require('./../js/venue.js').Venue;
+var location;
 
  function codeAddress(address) {
    var geocoder = new google.maps.Geocoder();
      geocoder.geocode( { 'address': address}, function(results, status) {
        if (status == google.maps.GeocoderStatus.OK) {
          var latlng = results[0].geometry.location;
+         location = latlng;
          var mapOptions = {
-           zoom: 14,
+           zoom: 13,
            center: latlng
          }
          map = new google.maps.Map(document.getElementById("map"), mapOptions);
@@ -33,17 +35,17 @@ $(document).ready(function(){
 
 
 function createMarker(venue){
-  var latlng = new google.maps.LatLng(venue.lat, venue.lng);
-  console.log(latlng);
-  console.log(map);
+  var latlng = new google.maps.LatLng(venue.lat, venue.long);
   var marker = new google.maps.Marker({
     map: map,
-    position: {lat: 59.327, lng: 18.067}
+    position: latlng
   });
-  map.setCenter(latlng);
-  console.log(marker);
-  var infowindow = new google.maps.InfoWindow();
-  infowindow.setContent(venue.name);
+  var infowindow = new google.maps.InfoWindow({
+    content: venue.name
+  });
+  marker.addListener('click', function() {
+   infowindow.open(map, marker);
+ });
 }
 
 function createUrl(city, query){
@@ -51,23 +53,15 @@ function createUrl(city, query){
   replace('%CLIENT_SECRET%', clientKey).
   replace('%CITY%', city).
   replace('%query%', query);
-  console.log(url);
   return url;
 }
 
 function apiCall(url){
   $.get(url).then(function(response){
-    console.log(response);
-    var current = response.response.groups[0].items[0].venue;
-    var name = current.name;
-    var rating = current.rating;
-    var price = current.price.message;
-    var lat = current.location.lat;
-    var lng = current.location.lng;
-    var address = current.location.address;
-    var venue = new Venue(name, rating, price, lat, lng, address);
-    console.log(venue);
-    createMarker(venue);
-
+    for(var i=0; i<10; i++){
+      var current = response.response.groups[0].items[i].venue;
+      var venue = new Venue(current.name, current.rating, current.price.message, current.location.lat, current.location.lng, current.location.address);
+      createMarker(venue);
+    }
   })
 }
