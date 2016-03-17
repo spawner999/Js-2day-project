c7e701ab2c2ec222ed5ecaa25c21333f411d6d1b
lorenzo ferrario <student@epicodus.com>
2016-03-17 15:16:54 -0700

    Add wikipedia api

--- js/api-helpers.js
+++ js/api-helpers.js
@@ -2,6 +2,8 @@
 var clientId = '44ZBUPBKJLBXBM0IJRZPSM34YPUKNSE2VQCNL041GNFIWRSO';
 var clientKey = 'RPHMMDSE0DQIAY4XZ34WN1ZRTUV250NCNIX05N1WMSRODPJ2';
 var template = 'https://api.foursquare.com/v2/venues/explore?client_id=%CLIENT_ID%&client_secret=%CLIENT_SECRET%&v=20130815&near=%CITY%&venuePhotos=1&query=%query%';
+var venueTemplate = '<div class="venue"><div class="card"><div class="face front"><div class="venue__img"><div class="venue__title"><h1>%NAME%</h1></div><h4>%RATING%</h4></div><div class="venue__info"><h3>%ADDRESS%</h3><i class="fa fa-star"></i></div></div><div class="face back"><p>lorem ipusm</p></div></div></div>';
+var wikiUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&exsentences=7&explaintext=&titles=%QUERY%&links&redirects';
 var Venue = require('./../js/venue.js').Venue;
 var markers = [];
 var infowindow;
@@ -22,6 +24,70 @@ exports.createMap = function(address) {
     });
   };
 
+
+exports.createUrl = function(city, query){
+  var url = template.replace('%CLIENT_ID%', clientId).
+  replace('%CLIENT_SECRET%', clientKey).
+  replace('%CITY%', city).
+  replace('%query%', query);
+  return url;
+};
+
+exports.createWikiUrl = function(query){
+  var url = wikiUrl.replace('%QUERY%', query);
+  return url;
+};
+
+exports.apiCall = function(url, callback){
+  $.get(url).then(function(response){
+    callback(response);
+  })
+};
+
+exports.venueCallback = function(response){
+  if(markers){
+    clearMarkers(markers);
+  }
+  for(var i=0; i<10; i++){
+    var current = response.response.groups[0].items[i].venue;
+    var venue = new Venue(current.name, current.rating, current.location.lat, current.location.lng, current.location.address, current.photos.groups[0].items[0].suffix);
+    var marker = createMarker(venue);
+    markers.push(marker);
+    var card = createCard(venue);
+    $('#slider').append(card);
+  }
+  addListener();
+}
+
+exports.wikiCallback = function(url){
+  $.ajax({
+  url: url,
+  dataType: "jsonp",
+  success: function (data) {
+    var object = data.query.pages;
+    $('#wiki').append(object[Object.keys(object)[0]].extract);
+  }
+});
+}
+
+function createCard(venue){
+  var card = venueTemplate.replace('%NAME%', venue.name).
+  replace('%ADDRESS%', venue.address).
+  replace('%RATING%', venue.rating);
+  return card;
+}
+
+function addListener(){
+  $('.venue__img').click(function(){
+    var current = $(this).parents('.venue').get(0);
+    $(current).find('.card').addClass('flipped')
+    .mouseleave(function(){
+      $(current).find('.card').removeClass('flipped');
+    });
+    return false;
+  });
+}
+
 createMarker = function(venue){
   var latlng = new google.maps.LatLng(venue.lat, venue.long);
   var marker = new google.maps.Marker({
@@ -37,62 +103,20 @@ createMarker = function(venue){
     setTimeout(function() {
       marker.setAnimation(false);
     }, 2050);
-   infowindow.open(map, marker);
- });
- return marker;
+    infowindow.open(map, marker);
+  });
+  return marker;
 };
 
 clearMarkers = function(markers) {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
+  markers = [];
 }
 
 clearInfoWindows = function(markers) {
   for (var i = 0; i < markers.length; i++) {
     infowindow.open(map, markers[i]);
   }
-}
-
-exports.createUrl = function(city, query){
-  var url = template.replace('%CLIENT_ID%', clientId).
-  replace('%CLIENT_SECRET%', clientKey).
-  replace('%CITY%', city).
-  replace('%query%', query);
-  return url;
-};
-
-exports.apiCall = function(url){
-  $.get(url).then(function(response){
-    console.log(response);
-    if(markers){
-      clearMarkers(markers);
-    }
-    for(var i=0; i<10; i++){
-      var current = response.response.groups[0].items[i].venue;
-      var venue = new Venue(current.name, current.rating, current.location.lat, current.location.lng, current.location.address, current.photos.groups[0].items[0].suffix);
-      var marker = createMarker(venue);
-      markers.push(marker);
-      var card = createCard(venue);
-      console.log(card);
-      $('#slider').append(card);
-      $('.venue__img').click(function(){
-        var current = $(this).parents('.venue').get(0);
-        $(current).find('.card').addClass('flipped')
-        .mouseleave(function(){
-          $(current).find('.card').removeClass('flipped');
-      });
-return false;
-});
-    }
-  })
-};
-
-var venueTemplate = '<div class="venue"><div class="card"><div class="face front"><div class="venue__img"><div class="venue__title"><h1>%NAME%</h1></div><h4>%RATING%</h4></div><div class="venue__info"><h3>%ADDRESS%</h3><i class="fa fa-star"></i></div></div><div class="face back"><p>lorem ipusm</p></div></div></div>'
-
-function createCard(venue){
-  var card = venueTemplate.replace('%NAME%', venue.name).
-  replace('%ADDRESS%', venue.address).
-  replace('%RATING%', venue.rating);
-  return card;
 }
--- js/main-interface.js
+++ js/main-interface.js
@@ -1,24 +1,29 @@
 var map;
+var address;
 var createMap = require('./../js/api-helpers.js').createMap;
 var apiCall = require('./../js/api-helpers.js').apiCall;
 var createUrl = require('./../js/api-helpers.js').createUrl;
 var clearMarkers = require('./../js/api-helpers.js').clearMarkers;
-var address;
+var venueCallback = require('./../js/api-helpers.js').venueCallback;
+var wikiCallback = require('./../js/api-helpers.js').wikiCallback;
+var createWikiUrl = require('./../js/api-helpers.js').createWikiUrl;
 
 
 $(document).ready(function(){
-
-
   $('#token').submit(function(event){
     event.preventDefault();
+    $('#slider').empty();
+    $('#wiki').empty();
     $('#category').prop('selectedIndex',0);
     address = $('#city').val();
     createMap(address);
+    wikiCallback(createWikiUrl(address));
   });
   $('#category').change(function() {
+    $('#slider').empty();
     var query = $('#category').val();
     if(query){
-      apiCall(createUrl(address, query));
+      apiCall(createUrl(address, query), venueCallback);
     }
 });
 });
